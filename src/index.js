3949bac5526a0b288c909db15dd02d4c4f074fd2
Ultron <ultrondev61@gmail.com>
2018-04-12 18:47:25 +0500

    Clock App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+setInterval(function () {
+    ReactDOM.render(
+        <h2>Time Now:- {new Date().toLocaleTimeString()}</h2>,
+        document.getElementById('root')
+    );
+});
