f5afac8c6edcd8f37599a7e3bd6da05cb21bf9d3
Ultron <ultrondev61@gmail.com>
2018-04-17 06:56:40 +0500

    'Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-setInterval(function () {
-    ReactDOM.render(
-        <h2>Time Now:- {new Date().toLocaleTimeString()}</h2>,
-        document.getElementById('root')
-    );
-});
+function Cartoon(props) {
+    return <h1>Cartoon name is {props.name} on {props.show}</h1>
+}
+
+function Show() {
+    return <div>
+        <Cartoon name={'Pikachu'} show={'Pokemon'}/>
+        <Cartoon name={'Jasmine'} show={'Aladdin'}/>
+    </div>
+}
+
+// class Cartoon extends React.Component {
+//     render() {
+//         return <h1>Cartoon name is {this.props.name}</h1>
+//     }
+// }
+
+ReactDOM.render(
+    <Show/>,
+    document.getElementById('root')
+);
